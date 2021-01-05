9a9d456bb3e23fc0e9e7d5c7b892bfa514e41397
Hriddhiman17 <hriddhiman2008@gmail.com>
2021-01-05 15:15:13 +0530

    commit

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,34 +1,39 @@
 import React, { Component } from 'react';
 import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
 import Header from '../components/AppHeader';
+import db from '../config';
 
 export default class Welcome extends Component {
   constructor(){
     super();
     this.state = {
-      phone : '',
+      phone: '',
+      card: '',
     }
   }
-  // showCardNoInput(){
-  //   if(this.state.phone.length === 10){
-  //     return(
-  //       <View>
-  //         <Text>hsssssssssssssssssss</Text>
-  //       </View>
-  //     )
-  //   }else{
-  //     return(
-  //       <View>
-  //         <Text>ssssssssssss</Text>
-  //       </View>
-  //     )
-  //   }
-  // }
+  createAProfile(){
+    db.collection("voters").doc(this.state.phone).set({
+      'Phone': this.state.phone,
+    })
+    db.collection("voters").doc(this.state.card).set({
+      'CardNumber': this.state.card,
+    })
+  }
   render() {
     return (
       <View>
         <Header/>
-        <Text style={styles.text}>Welcome to the Vote App. Please Enter your phone no. here </Text>
+        <Text style={styles.text}>Welcome to the Vote From Home App. This App would help to make your vote easy, safe, and from home.</Text>
+        <Text>Please Enter your Name here-</Text>
+        <TextInput 
+            style = {styles.inputBox}
+            placeholder = "Your Name"
+            keyboardType= "default"
+            onChangeText = {text=>
+              this.setState({
+                phone: text
+              })
+            }/>
         <TextInput 
             style = {styles.inputBox}
             placeholder = "Phone number"
@@ -37,12 +42,20 @@ export default class Welcome extends Component {
               this.setState({
                 phone: text
               })
-            }
-        />
-        <Text></Text>
-        <TouchableOpacity 
+            }/>
+        <Text style={styles.text}>Enter your card no. here-</Text>
+        <TextInput
+            style = {styles.inputBox}
+            placeholder = "Card number"
+            keyboardType= "numeric"
+            onChangeText = {text=>
+              this.setState({
+                card: text
+              })
+            }/>
+        <TouchableOpacity
             style = {styles.submitButton}
-            onPress = {}>
+            onPress = {()=> {this.createAProfile}}>
             <Text style = {{fontSize:22}}>Submit</Text>
         </TouchableOpacity>
       </View>
